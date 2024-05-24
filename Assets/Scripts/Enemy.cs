80fb553ca39ccd6d6cbd6d4fa1a8976ae3034123
ubuntu2000 <hongbien1989hp@gmail.com>
2024-05-24 22:16:48 +0700

    Update v3.01
    Het bai 13

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+namespace GameBasic
+{
         
+    public class Enemy : MonoBehaviour, IComponentCheck
+    {
+        Animator m_anim;
+        Rigidbody2D m_rb;
+        Player m_player;
+        public float movespeed;
+        public float EnemyDistance;
+
+        private void Awake()
+        {
+            m_anim = GetComponent<Animator>();
+            m_rb = GetComponent<Rigidbody2D>();
+            m_player = FindObjectOfType<Player>();
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        public bool IsComponentsNull()
+        {
+            return m_anim == null || m_player == null || m_rb == null;
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (IsComponentsNull()) return;
+            float PlayerDistance = Vector2.Distance(m_player.transform.position, transform.position);
+           if ( PlayerDistance <= EnemyDistance)
+            {
+                m_anim.SetBool(Const.ATTACK_ANIM, true);
+                m_rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                m_rb.velocity = new Vector2(-movespeed, m_rb.velocity.y);
+            }
+        }
+        public void Die()
+        {
+            if (IsComponentsNull()) return;
+           
+             m_anim.SetTrigger(Const.DEAD_ANIM);
+             m_rb.velocity = Vector2.zero;
+            gameObject.layer = LayerMask.NameToLayer(Const.DEAD_ANIM);
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,55 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace GameBasic
 {
 
-    Animator m_anim;
 
-    public float Time_RateAttack;
-    float m_CurTime_RateAttack;
-    bool m_IsAttack;
+    public class Player : MonoBehaviour, IComponentCheck 
+    {
 
-    private void Awake()
-    {
-        m_anim = GetComponent<Animator>();
-        m_CurTime_RateAttack = Time_RateAttack;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-        
+        Animator m_anim;
+
+        public float Time_RateAttack;
+        float m_CurTime_RateAttack;
+        bool m_IsAttack;
+        bool m_IsDead;
+
+        private void Awake()
+        {
+            m_anim = GetComponent<Animator>();
+            m_CurTime_RateAttack = Time_RateAttack;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+
+        }
+
+        public bool  IsComponentsNull()
+        {
+            return m_anim == null;
+        }
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (IsComponentsNull()) return;
+            if (Input.GetMouseButtonDown(0) && !m_IsAttack)
+            {
+         
+                m_anim.SetBool(Const.ATTACK_ANIM, true);
+                m_IsAttack = true;
+
+            }
+            if (m_IsAttack)
+            {
+                m_CurTime_RateAttack -= Time.deltaTime;
+                if (m_CurTime_RateAttack <= 0)
+                {
+                    m_IsAttack = false;
+                    m_CurTime_RateAttack = Time_RateAttack;
+                }
+            }
+
+
+        }
+        public void ReSetAttackAnim()
+        {
+            if (IsComponentsNull()) return;
+            {
+                m_anim.SetBool(Const.ATTACK_ANIM, false);
+            }
+        }
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (IsComponentsNull()) return;
+            if(col.CompareTag(Const.ENEMY_WEAPON_TAG) && !m_IsDead)
+            {
+                m_anim.SetTrigger(Const.DEAD_ANIM);
+                m_IsDead = true;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0) && !m_IsAttack)
-        {
-            if (m_anim)
-            
-               m_anim.SetBool(Const.ATTACK_ANIM, true);
-               m_IsAttack = true;
-            
-        }
-        if(m_IsAttack)
-        {
-             m_CurTime_RateAttack -= Time.deltaTime;
-             if (m_CurTime_RateAttack <= 0)
-             {
-                 m_IsAttack = false;
-                 m_CurTime_RateAttack = Time_RateAttack;
-             }
-        }
-        
-             
-    }
-    public void ReSetAttackAnim()
-    {
-        if(m_anim)
-        {
-            m_anim.SetBool(Const.ATTACK_ANIM, false);
-        }
-    }
 }
